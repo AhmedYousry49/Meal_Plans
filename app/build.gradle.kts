336e8b969c3078bc068da6b154bbe220b3af1c72
ahmedyousry49 <ahmedyousry449@gmail.com>
2025-05-04 09:49:35 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,62 @@
+
+import com.google.firebase.appdistribution.gradle.firebaseAppDistribution
+
+
+plugins {
+    alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
+    id("com.google.firebase.appdistribution")
+
+}
+
+android {
+    namespace = "com.iti.uc3.mealplans"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.iti.uc3.mealplans"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        getByName("release") {
+            firebaseAppDistribution {
+                appId = "1:777663286370:android:6c4af0099cc9fae638953b"
+                artifactType = "APK"
+//                releaseNotesFile = "path/to/releasenotes.txt"
+                testers = "ahmeddly30971@gmail.com"
+
+
+            }
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
+
+}
